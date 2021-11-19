5181807937d04913eda9d5769d72aa912e4fc4f1
JG <94547572+jangroeschel@users.noreply.github.com>
2021-11-19 18:17:53 -0300

    t

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -23,38 +23,30 @@ function blog_posts() {
     // keeping it static, can be fetched from a blog dynamically as well
     let posts = [
         {
-            url: 'https://www.nagekar.com/2017/02/trip-to-bramhatal-uttarakhand.html',
-            title: 'Trek To Bramhatal (Uttarakhand)',
+            url: 'https://www.linkedin.com/in/jangroeschel/',
+            title: 'Placeholder authorship description 1',
         },
         {
-            url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
+            url: 'https://www.linkedin.com/in/jangroeschel/',
+            title: 'Placeholder authorship description 1',
         },
         {
-            url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
+            url: 'https://www.linkedin.com/in/jangroeschel/',
+            title: 'Placeholder authorship description 1',
         },
         {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
+            url: 'https://www.linkedin.com/in/jangroeschel/',
+            title: 'Placeholder authorship description 1',
         },
         {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
+            url: 'https://www.linkedin.com/in/jangroeschel/',
+            title: 'Placeholder authorship description 1',
         },
     ];
 
     let post_html = [];
 
     for(let post of posts) {
-
-        let tags;
-        
-        if(post.tags) {
-            tags = post.tags.map(tag => {
-                return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`
-            })
-        }
 
         let post_template = `
         <div class="blog-post" onclick="blog_link_click('${post.url}');">
@@ -76,11 +68,11 @@ function blog_posts() {
 
     // for the more posts link
     let post_template = `
-    <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
+    <div class="blog-post more-blogs" onclick="blog_link_click('https://www.linkedin.com/in/jangroeschel/');">
 
         <div class="blog-link">
 
-            <h3><a href="https://www.nagekar.com">Visit the blog for more posts</a></h3>            
+            <h3><a href="https://www.linkedin.com/in/jangroeschel/">Visit my LinkedIn in for more posts</a></h3>            
 
         </div>
 
